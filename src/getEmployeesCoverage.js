3e5745049bbfaa3876d9cbd2a53bd525a5dfab1a
Vinicius <visouza0@gmail.com>
2022-08-29 14:40:22 -0300

    adiciona getEmployeesCoverage ainda falta refatorar o projeto

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const getName = data.employees.map((name) => {
+  const eInfo = {
+    id: name.id,
+    fullName: `${name.firstName} ${name.lastName}`,
+    species: name.responsibleFor.map((IdAnimal) =>
+      data.species.find((info) => info.id === IdAnimal).name),
+    locations: name.responsibleFor.map((IdAnimal) =>
+      data.species.find((info) => info.id === IdAnimal).location),
+  };
+  return eInfo;
+});
+
+function getEmployeesCoverage(obj = getName) {
+  const getCheck = getName.find((name) =>
+    name.fullName.includes(obj.name) || name.id === obj.id);
+  if (obj.name || obj.id) {
+    if (!getCheck) {
+      throw new Error('Informações inválidas');
+    }
+    return getCheck;
+  }
+  return obj;
 }
 
 module.exports = getEmployeesCoverage;
